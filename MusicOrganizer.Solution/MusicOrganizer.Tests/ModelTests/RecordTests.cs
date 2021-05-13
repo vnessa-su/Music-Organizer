41b7f6211e050b44f7896a6fa9e1cabb41e3319d
Brandon-Magofna <magofna68@gmail.com>
2021-05-13 10:43:55 -0700

    add find method and complete testing on getall method
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>

--- MusicOrganizer.Solution/MusicOrganizer.Tests/ModelTests/RecordTests.cs
+++ MusicOrganizer.Solution/MusicOrganizer.Tests/ModelTests/RecordTests.cs
@@ -35,11 +35,31 @@ namespace MusicOrganizer.Tests
     [TestMethod]
     public void GetAll_ReturnsEmptyList_recordTitles()
     {
-      List<Record> newList = new List<Record> {};
+      List<Record> newList = new List<Record> { };
 
       List<Record> result = Record.GetAll();
 
       CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsRecords_recordTitles()
+    {
+      string recordTitle1 = "Thrust";
+      string recordTitle2 = "Save Me";
+      Record newRecord1 = new Record(recordTitle1);
+      Record newRecord2 = new Record(recordTitle2);
+      List<Record> newList = new List<Record> { newRecord1, newRecord2 };
+
+      List<Record> result = Record.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
+    [TestMethod]
+
+    public void Find_ReturnsAccurateRecord_Record()
+    {
+      
     }
   }
 }
